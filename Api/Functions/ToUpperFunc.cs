5b70b50e74022a52406c2352e74282145ac7e9ca
Nikola Nedevski <nikola.nedevski@gmail.com>
2025-03-21 19:53:14 +0200

    init

---
+++ Api/Functions/ToUpperFunc.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+namespace Api.Functions;
+
+class ToUpperFunc
+{
+    public APIGatewayHttpApiV2ProxyResponse Handler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    {
+        request.QueryStringParameters.TryGetValue("input", out var input);
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            Body = input?.ToUpper() + "WAKE UP",
+            StatusCode = 200
+        };
+    }
+}
